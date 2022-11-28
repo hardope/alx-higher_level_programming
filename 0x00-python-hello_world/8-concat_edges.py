95dcacf01833f891610cf4cd5b89fc7fbe8ae99a
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-11-28 18:07:46 +0000

    changes

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+str = str[39:67] + str[107:112] + str[:6]
+print("object-oriented programming with Python")
