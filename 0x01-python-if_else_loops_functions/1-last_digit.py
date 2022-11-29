e1910eed1fdb6658ad4835845613d5f72d343a7b
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-11-29 09:10:45 +0000

    Created file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0 and str(number)[-1] != "0":
+    sign = "-"
+else:
+    sign = ""
 number = f"{number}"
-print(f"Last digit of {number} is {number[-1]}")
+print(f"Last digit of {number} is {sign}{number[-1]}")
