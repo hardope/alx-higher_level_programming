8e6dc912691d684891f00ff44065e38a6ecf42c8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-11-29 16:08:51 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,6 @@
-code 6-print
+#!/usr/bin/python3
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
+    else:
+        print("{}{}".format(int(number / 10), number % 10))
