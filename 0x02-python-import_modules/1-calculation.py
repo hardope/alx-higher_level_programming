887a0c8375e9e65aee1cd4e98f477b2a18e5dd2b
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-01 12:29:48 +0000

    calculate

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import div
+    from calculator_1 import mul
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
