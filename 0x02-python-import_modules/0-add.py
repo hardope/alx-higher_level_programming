ca52364f788cd962dbcffa2e62c847413ef6f76e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-01 11:33:26 +0000

    file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
-def main():
-    """My Main Function
-    To use the add function
-    """
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    main()
