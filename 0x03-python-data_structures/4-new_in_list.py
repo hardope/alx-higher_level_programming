d2da6dae2ff5f3bff98dc7845fd5a4df8fded4d2
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-05 12:56:00 +0000

    Files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    if idx > len(my_list)-1 or idx < 0:
+    if idx > len(my_list)-1 or idx <= 0:
         return my_list
     else:
         new_list = my_list
