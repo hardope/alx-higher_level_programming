a2081068c9ffdf23503fd5ce17b7d78a61bd83cc
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-05 08:22:06 +0000

    chmod

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,7 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    else
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
