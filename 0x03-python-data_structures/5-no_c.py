753d49029d1fa92e44e0dc6b003bc0ce27f096dd
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-05 13:08:35 +0000

    Files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def no_c(my_string):
     new_string = ""
 
     for i in my_string:
-        if i == 'c':
+        if i == 'c' or i == "C":
             pass
         else:
             new_string+=i
