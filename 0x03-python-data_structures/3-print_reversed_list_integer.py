6d7253cd5cd55c00d5ab6d3d62e12d50756fb22c
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-05 12:43:05 +0000

    Files

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
+    if not my_list:
         pass
     else
         my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+        for i in my_list:
+            print("{:d}".format(i))
