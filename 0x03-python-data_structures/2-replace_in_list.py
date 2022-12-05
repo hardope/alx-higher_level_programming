7c57c1a2df7a505e84892295f9819c06a1341422
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-05 08:05:54 +0000

    chmod

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx > len(my_list)-1 or idx < 0:
+        pass
+    else:
+        my_list[idx] = element
+    return my_list
