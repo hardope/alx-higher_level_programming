044553649f9d98958f8d1e71e0259110a028ecb1
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-07 16:40:05 +0000

    Files

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    return None if not a_dictionary
-    return (max(a_dictionary, key=a_dictionary.get))
+    return None if not a_dictionary else (max(a_dictionary, key=a_dictionary.get))
