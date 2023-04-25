efc8b1e6873082861d39b4aa87e39d8630bc8d3c
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-25 22:14:57 +0000

    Statuscode

---
+++ 2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Status Of Get request
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
+});
