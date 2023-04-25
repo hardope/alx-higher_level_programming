8b5d6ebf3269d7c71effe9fde9b3f7023c9a817a
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-25 22:09:26 +0000

    Updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filename = process.argv[2];
-const content = process.argv[3];
 
-fs.writeFile(filename, content, 'utf-8',
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
   function (err) {
     if (err) {
       console.log(err);
