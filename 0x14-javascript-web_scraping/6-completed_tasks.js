5c421d709547dfe805fa0dc9ee9a85b617c4622e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-25 23:02:17 +0000

    Updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, body) {
+request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   }
