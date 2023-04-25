84e539d48dba241552decb33e5b399b6bd72fdb4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-25 21:54:42 +0000

    1-writeme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
+// Reads and prints content
 
-const fs = require('fs');
-const filename = process.argv[2];
-fs.readFile(filename, 'utf-8',
-  (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+const argv = process.argv;
+let fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
-  });
+  }
+});
